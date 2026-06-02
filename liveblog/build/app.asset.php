05f9ac9b0fe18cb3c8984e113ef2f160929f61ba
Sean Murphy <sean@iamseanmurphy.com>
2026-06-02 15:38:30 +0000

    1.12.1

--- liveblog/build/app.asset.php
+++ liveblog/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-i18n'), 'version' => 'e5d010159258bc09f6cb');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-i18n'), 'version' => '91159093183c64c6abc5');
